e5b59d06b8ce56db614337dba4b0d422e8ea3167
GitGomi <gomi1203@gmail.com>
2018-02-01 06:17:02 -0800

    PracticaDos

---
+++ PracticaDos/PracticaDos/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaDos.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
+            return View();
+        }
+
+        public ActionResult Reporte()
+        {
+            return View();
+        }
+
+        public ActionResult Informacion()
+        {
+            return View();
+        }
+
+    }
+}
